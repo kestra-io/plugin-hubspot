526ee07995870d550b6d70d06afab0ee1b8c24a9
ben8t <pimpaudben@gmail.com>
2025-12-30 15:38:22 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/hubspot/companies/package-info.java
+++ src/main/java/io/kestra/plugin/hubspot/companies/package-info.java
@@ -1,6 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for managing companies using HubSpot API.",
-    categories = PluginSubGroup.PluginCategory.OTHER
+    categories = PluginSubGroup.PluginCategory.OTHER,
+    categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.hubspot.companies;
 
