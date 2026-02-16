43978606ca6d6f7d0b4d4bb46b25a8c1f7e31f69
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-16 07:02:45 +0100

    chore: update plugin categories (#52)

--- src/main/java/io/kestra/plugin/hubspot/companies/package-info.java
+++ src/main/java/io/kestra/plugin/hubspot/companies/package-info.java
@@ -1,6 +1,8 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for managing companies using HubSpot API.",
-    categories = PluginSubGroup.PluginCategory.OTHER
+    categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.hubspot.companies;
 
--- src/main/java/io/kestra/plugin/hubspot/deals/package-info.java
+++ src/main/java/io/kestra/plugin/hubspot/deals/package-info.java
@@ -1,6 +1,8 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for managing deals using HubSpot API.",
-    categories = PluginSubGroup.PluginCategory.OTHER
+    categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.hubspot.deals;
 
